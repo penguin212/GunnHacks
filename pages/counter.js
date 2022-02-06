aea9cee96b9077c16c7917b7589046c3d319c4df
Kevin Yu <kevinyu567@gmail.com>
2022-02-05 17:18:03 -0800

    Counter and state

---
+++ pages/counter.js
@@ -0,0 +1,18 @@
+import {useState} from 'react';
+import Container from '../components/Container';
+
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+    const [on, setOn] = useState(false);
+
+    return (
+        <Container>
+            <h1>The current count is {count}</h1>
+            <button onClick={() => setCount(count + 1)}>Click me!</button>
+            <button onClick={() => setOn(!on)}>
+                The nuclear reactor is {on ? 'activated' : 'deactivated'}
+            </button>
+        </Container>
+    )
+}
